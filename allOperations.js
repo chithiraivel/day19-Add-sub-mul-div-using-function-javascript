2fc5eaadfabb1f73020a80372198a58457acd99d
chithiraivel <chithiraivels11@gmail.com>
2022-06-24 12:16:48 +0530

    operation.

---
+++ allOperations.js
@@ -0,0 +1,28 @@
+
+function add(a,b){
+
+    return a + b;
+}
+console.log(add(10,20));
+
+
+function sub(a,b){
+
+    return a - b;
+}
+console.log(sub(30,20));
+
+
+function mul(a,b){
+
+    return a * b;
+}
+console.log(mul(10,20));
+
+
+
+function div(a,b){
+
+    return a / b;
+}
+console.log(div(10,2));
